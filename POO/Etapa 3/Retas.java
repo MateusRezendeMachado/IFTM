8d8895432e1084f17ecad713f6b2bca4d05897ed
MateusRezendeMachado <124397254+MateusRezendeMachado@users.noreply.github.com>
2025-10-14 21:09:12 -0300

    Create Retas.java

---
+++ POO/Etapa 3/Retas.java
@@ -0,0 +1,24 @@
+public class Retas {
+    private double x1, y1, x2, y2;
+    public static int cont = 0;
+
+    
+    public Retas(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        cont++;
+    }
+
+    
+    public double comprimento() {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    
+    public String exibe() {
+        return String.format("Reta: (%.2f, %.2f) até (%.2f, %.2f) | Comprimento: %.2f",
+                x1, y1, x2, y2, comprimento());
+    }
+}
